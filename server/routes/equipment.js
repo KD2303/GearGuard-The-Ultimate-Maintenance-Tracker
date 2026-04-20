5bc6640b1b1fbbefab952838a4b4b1efe68fc569
pranavms09 <pranavms09@gmail.com>
2026-04-20 18:51:52 +0530

    fix: apply JWT protect middleware to all API routes

--- server/routes/equipment.js
+++ server/routes/equipment.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const equipmentController = require('../controllers/equipmentController');
+const protect = require('../middleware/auth');
+
+router.use(protect);
 
 router.get('/', equipmentController.getAllEquipment);
 router.get('/:id', equipmentController.getEquipmentById);
