cdf6474af8827d4c64ae85f104375f2f54ac6ba9
Prateek Amar Batham <om31batham10@gmail.com>
2026-06-15 23:10:28 +0530

    feat: Downtime Geolocation Heatmap API

--- server/routes/map.js
+++ server/routes/map.js
@@ -6,11 +6,13 @@ const verifyToken = require('../middleware/auth');
 const {
   getFloorPlan,
   bulkUpdateEquipmentCoordinates,
+  getDowntimeHeatmap
 } = require('../controllers/mapController');
 
 router.use(verifyToken);
 
 router.get('/floor-plan', getFloorPlan);
 router.put('/equipment/coordinates', authorizeRoles('Admin', 'Manager'), bulkUpdateEquipmentCoordinates);
+router.get('/downtime-heatmap', getDowntimeHeatmap);
 
 module.exports = router;
